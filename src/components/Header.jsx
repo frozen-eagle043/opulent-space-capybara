16eaa25d607b7f5f2c39546f0772bd3374521fbe
Shivam Devaser <55057194+frozen-eagle043@users.noreply.github.com>
2024-01-13 15:56:05 +0000

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Banner from './Banner'
+const Header = () => {
+  return (
+    <div>
+        <Banner/>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Header
