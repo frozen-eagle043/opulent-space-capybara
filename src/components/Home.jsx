0fe00c4d78bd26a04f2dffa1046e642c84276963
Shivam Devaser <55057194+frozen-eagle043@users.noreply.github.com>
2024-01-14 11:07:00 +0000

    Second Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-
+import Body from './Body'
 const Home = () => {
   return (
     <div>
         <Header />
         Home
+        <Body />
         <Footer />
     </div>
   )
