8f0c17ac10cdb5b070b3c61d5f7449ea3056ee59
SHIVAM <shivamdevaser934@gmail.com>
2024-02-16 18:06:54 +0530

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,119 +1,47 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import CallForPapers from "./CallForPapers";
-//import "./Navbar.css"; // Make sure to create a corresponding CSS file
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-const Navbar = () => {
-  const [isSubmissionOpen, setSubmissionOpen] = useState(false);
-  const [isChallengesOpen, setChallengesOpen] = useState(false);
-  const [isCommitteeOpen, setCommitteeOpen] = useState(false);
-  const [isProgrammeOpen, setProgrammeOpen] = useState(false);
-
+const NavbarComponent = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <Link to="/">
-          <img src="/path-to-your-logo.png" alt="Logo" />{" "}
-          {/* Adjust the src to your logo's path */}
-        </Link>
-      </div>
-
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-
-        {/* Submission Dropdown */}
-        <li onClick={() => setSubmissionOpen(!isSubmissionOpen)}>
-          <Link>Submission +</Link>
-          {isSubmissionOpen && (
-            <ul className="dropdown">
-              <li>
-                <Link to="/call-for-paper">Call For Paper</Link>
-              </li>
-              <li>
-                <Link to="/important-dates">Important Dates</Link>
-              </li>
-              <li>
-                <Link to="/author-guidelines">Author Guidelines</Link>
-              </li>
-              <li>
-                <Link to="/camera-ready-submission">
-                  Camera Ready Submission
-                </Link>
-              </li>
-              <li>
-                <Link to="/presentation-guidelines">
-                  Presentation Guidelines
-                </Link>
-              </li>
-            </ul>
-          )}
-        </li>
-
-        {/* Challenges Dropdown */}
-        <li onClick={() => setChallengesOpen(!isChallengesOpen)}>
-          <Link>Challenges +</Link>
-          {isChallengesOpen && (
-            <ul className="dropdown">
-              <li>
-                <Link to="/call-for-challenge">Call For Challenge</Link>
-              </li>
-              <li>
-                <Link to="/important-dates-challenge">Important Dates</Link>
-              </li>
-              <li>
-                <Link to="/challenge-author-guidelines">Author Guidelines</Link>
-              </li>
-            </ul>
-          )}
-        </li>
-
-        {/* Committee Dropdown */}
-        <li onClick={() => setCommitteeOpen(!isCommitteeOpen)}>
-          <Link>Committee +</Link>
-          {isCommitteeOpen && (
-            <ul className="dropdown">
-              <li>
-                <Link to="/organizing-committee">Organizing committee</Link>
-              </li>
-              <li>
-                <Link to="/advisory-committee">Advisory Committee</Link>
-              </li>
-            </ul>
-          )}
-        </li>
-
-        {/* Programme Dropdown */}
-        <li onClick={() => setProgrammeOpen(!isProgrammeOpen)}>
-          <Link>Programme +</Link>
-          {isProgrammeOpen && (
-            <ul className="dropdown">
-              <li>
-                <Link to="/our-speakers">Our Speakers</Link>
-              </li>
-              <li>
-                <Link to="/travel-grant">Travel Grant</Link>
-              </li>
-              <li>
-                <Link to="/program-schedule">Program Schedule</Link>
-              </li>
-            </ul>
-          )}
-        </li>
-
-        <li>
-          <Link to="/registration">Registration</Link>
-        </li>
-        <li>
-          <Link to="/accommodation">Accommodation</Link>
-        </li>
-        <li>
-          <Link to="/award">Award</Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="light" expand="lg" className="py-3">
+      <Container fluid>
+        <Navbar.Brand href="#" className="me-auto fw-bold">
+          CVIP 2023
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="#" className="px-2">
+            Home
+          </Nav.Link>
+          <Nav.Link href="#" className="px-2">
+            Submission
+          </Nav.Link>
+          {/* Updated Challenges Link to include sub-links */}
+          <NavDropdown
+            title="Challenges"
+            id="navbarScrollingDropdown"
+            className="px-2"
+          >
+            <NavDropdown.Item href="#action3">A Challenge</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">B Challenge</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#" className="px-2">
+            Committee
+          </Nav.Link>
+          <Nav.Link href="#" className="px-2">
+            Programme
+          </Nav.Link>
+          <Nav.Link href="#" className="px-2">
+            Registration
+          </Nav.Link>
+          <Nav.Link href="#" className="px-2">
+            Accommodation
+          </Nav.Link>
+          <Nav.Link href="#" className="px-2">
+            Award
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
